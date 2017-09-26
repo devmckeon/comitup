f161abfbfef0e1d04af1551fc8b6bad9f3b5965e
David Steele <dsteele@gmail.com>
2017-09-26 12:40:24 -0400

    Make client methods late-binding

--- comitup/client.py
+++ comitup/client.py
@@ -10,34 +10,55 @@
 import dbus
 import sys
 
-bus = dbus.SystemBus()
+ciu_service = None
 
-try:
-    ciu_service = bus.get_object(
-                   'com.github.davesteele.comitup',
-                   '/com/github/davesteele/comitup'
-                  )
-except dbus.exceptions.DBusException:
-    print("Error connecting to the comitup D-Bus service")
-    sys.exit(1)
+def get_method(fn_nm):
+    global ciu_service
 
-ciu_state = ciu_service.get_dbus_method(
-                'state',
-                'com.github.davesteele.comitup'
-            )
-ciu_points = ciu_service.get_dbus_method(
-                'access_points',
-                'com.github.davesteele.comitup'
-             )
-ciu_delete = ciu_service.get_dbus_method(
-                'delete_connection',
-                'com.github.davesteele.comitup'
-             )
-ciu_connect = ciu_service.get_dbus_method(
-                'connect',
-                'com.github.davesteele.comitup'
-             )
-ciu_info = ciu_service.get_dbus_method(
-                'get_info',
-                'com.github.davesteele.comitup'
-             )
+    if not ciu_service:
+        bus = dbus.SystemBus()
+
+        try:
+            ciu_service = bus.get_object(
+                           'com.github.davesteele.comitup',
+                           '/com/github/davesteele/comitup'
+                          )
+        except dbus.exceptions.DBusException:
+            print("Error connecting to the comitup D-Bus service")
+            sys.exit(1)
+
+    return ciu_service.get_dbus_method(fn_nm, 'com.github.davesteele.comitup')
+
+
+def call_method(fn_name):
+    def _call_method(fn):
+        def wrapper(*args, **kwargs):
+            return get_method(fn_name)(*args, **kwargs)
+
+        return wrapper
+    return _call_method
+
+
+@call_method('state')
+def ciu_points(*args, **kwargs):
+    pass
+
+
+@call_method('access_points')
+def ciu_state(*args, **kwargs):
+    pass
+
+
+@call_method('delete_connection')
+def ciu_delete(*args, **kwargs):
+    pass
+
+
+@call_method('connect')
+def ciu_connect(*args, **kwargs):
+    pass
+
+
+@call_method('get_info')
+def ciu_info(*args, **kwargs):
+    pass
